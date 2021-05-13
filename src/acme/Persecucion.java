26f578240d72cc8d192f52fe20995f9007543e04
1DAW-B <1DAW-B@PC22>
2021-05-13 20:30:19 +0200

    "Móvil ACME v2.0"

---
+++ src/acme/Persecucion.java
@@ -0,0 +1,26 @@
+package acme;
+
+import java.io.IOException;
+
+public class Persecucion {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Primero inicializamos un smartphone ACME con un código de licencia válido
+		Smartphone miACMESmartphone = new Smartphone(2.0, "coyote_398978fdskj");
+		
+		// Localizamos el correcaminos más cercano.
+		String coordenadas=miACMESmartphone.locateRoadRunner(PROVINCIA, PAIS);
+		
+		// Preparamos la trampa
+                miACMESmartphone.prepareTrap(coordenadas);
+                
+                // Golpear al correcaminos con el voltaje deseado.
+		miACMESmartphone.zapRoadRunner(40);
+
+	}
+    private static final String PAIS = "Spain";
+    private static final String PROVINCIA = "Sevilla";
+        
+
+}
